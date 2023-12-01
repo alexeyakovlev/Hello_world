6507e8230e3d75ad37080d3a06a094875fe64929
“YakovlevAV” <alexyakovlev95@gmail.com>
2023-12-01 16:48:50 +0300

    add Project5

---
+++ Project5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите иимя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.Write("Ура, это же МАША!!!");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
